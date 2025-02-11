1fbb1db5f9c7b4abb27a8bbd81af5947f6086784
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2025-02-11 03:03:51 -0400

    retornando os dados do backend para frontend da tarefa que o usuário  após o usuário inserir uma nova tarefa

--- backend/src/services/task/CreateTasksServices.ts
+++ backend/src/services/task/CreateTasksServices.ts
@@ -1,3 +1,4 @@
+import { ResultSetHeader } from "mysql2";
 import connection from "../../connection/connection";
 
 interface CreateTasksPros {
@@ -9,8 +10,13 @@ class CreateTasksServices {
     if (!title) throw new Error("O campo tarefa não pode estar vazio");
     const query = "INSERT INTO tasks (title) VALUES (?)";
     try {
-      await connection.query(query, [title]);
-      return "Tarefa inserida com sucesso!";
+      const result = await connection.query<ResultSetHeader>(query, [title]);
+      const taskId = result[0].insertId;
+
+      const query_task = "SELECT * FROM tasks WHERE id = ? LIMIT 1";
+      const task = await connection.query(query_task, [taskId]);
+      console.log(task[0]);
+      return task[0];
     } catch (error) {
       throw new Error(
         error instanceof Error ? error.message : "Erro ao inserir tarefa."
