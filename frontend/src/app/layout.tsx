d9bd8ef6e28a55bce2ffe8208b1c741ca4e69fcd
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2025-02-11 03:36:17 -0400

    posicionando o toast no lado superior direito

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <>
-          <Toaster />
+          <Toaster position="top-right" reverseOrder={false} />
           {children}
         </>
       </body>
